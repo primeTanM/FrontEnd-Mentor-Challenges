c3998d76d23f5e23e018c8d1fc7d526861853a93
Tanmay <tanmaymisra619@gmail.com>
2021-07-29 15:01:41 +0530

    fixed button functionality

--- sunnyside-agency-landing-page/script.js
+++ sunnyside-agency-landing-page/script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementsByClassName('hamburger-button')
+const toggleButton = document.getElementsByClassName('togglebtn')[0]
 
-const navbarLinks = document.getElementsByClassName('navbar')
+const navbarLinks = document.getElementsByClassName('navbar')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
