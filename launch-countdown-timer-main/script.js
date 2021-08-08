92c24ad810881df0a5ccc33eefc976243255c4cf
Tanmay <tanmaymisra619@gmail.com>
2021-08-08 14:31:23 +0530

    initial commit

---
+++ launch-countdown-timer-main/script.js
@@ -0,0 +1,51 @@
+const timer = () => {
+    const countDate = new Date("September 10, 2021 00:00:00").getTime();
+    const now = new Date().getTime();
+
+    const difference = countDate - now;
+    
+    const seconds = 1000;  //milliseconds
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 60;
+
+    const displayDays = Math.floor(difference / days);
+    const displayHours = Math.floor((difference % days) / hours);
+    const displayMinutes = Math.floor((difference % hours) / minutes);
+    const displaySeconds = Math.floor((difference % minutes) / seconds);
+
+
+    
+    if (displayDays < 10) {
+        document.getElementById('days').innerText = '0' + displayDays;
+    }else {
+        document.getElementById('days').innerText = displayDays;
+    }
+
+
+    
+    if (displayHours < 10) {
+        document.getElementById('hours').innerText = '0' + displayHours;
+    }else {
+        document.getElementById('hours').innerText = displayHours;
+    }
+
+
+    
+    if (displayMinutes < 10) {
+        document.getElementById('minutes').innerText = '0' + displayMinutes;
+    }else {
+        document.getElementById('minutes').innerText = displayMinutes;
+    }
+
+
+    if (displaySeconds < 10) {
+        document.getElementById('seconds').innerText = '0' + displaySeconds;
+    }else {
+        document.getElementById('seconds').innerText = displaySeconds;
+    }
+    
+
+}
+
+setInterval(timer, 1000);
